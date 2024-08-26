87ba79e4df8b296980fadcd8ff403bd259ddd5cf
SeanFuXiao <fuxiaosean@gmail.com>
2024-08-26 13:02:05 -0400

    added skill wrapper box

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+            }
+        });
+    });
+    document.querySelectorAll('.fade-in-section').forEach(section => {
+        observer.observe(section);
+    });
+});
+
